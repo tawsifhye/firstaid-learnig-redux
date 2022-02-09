1ae982d297eb709025d544e733d79fed96975e66
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-02-09 23:19:59 +0600

    Slick carousel added and card added

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -1,17 +1,57 @@
 import { Box } from '@mui/system';
 import React from 'react';
+import PeopleIcon from '@mui/icons-material/People';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 import TagLine from './shared/Tagline';
+import { Button } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-const CourseCard = () => {
+const CourseCard = ({ course }) => {
+    const { id, title, coverImage, enrolledStudents, rating, regularPrice } = course;
+    console.log(course);
     return (
         <Box sx={{
-            mt: '30px',
+            my: '30px',
+            padding: '10px',
+            mr: '20px',
             background: '#FFFFFF',
             boxShadow: '0px 45.799px 106.864px rgba(234, 46, 16, 0.06)',
             borderRadius: '19.0829px'
         }}>
-            <img src="" alt="" />
-            <TagLine text={<div>Online Microsoft Excel Certification Bundles</div>} fontSize="30.2554px" />
+            <img style={{ width: '100%', marginBottom: '10px', borderRadius: '19.08px' }} src={coverImage} alt="" />
+            <TagLine text={<div>{title}</div>} fontSize="20px" />
+
+            <Box sx={{
+                mt: '30px',
+                display: 'flex',
+                justifyContent: 'space-between'
+            }}>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                    <PeopleIcon sx={{ mr: 1, color: '#EA2E10' }} />
+                    <span style={{ color: '#435770' }}>{enrolledStudents}</span>
+                </Box>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}>
+                    <MilitaryTechIcon sx={{ mr: 1, color: '#EA2E10' }} />
+                    <span style={{ color: '#435770' }}>CPD Accrediated</span>
+                </Box>
+            </Box>
+
+            <Button variant="outlined" sx={{
+                width: '100%',
+                mt: '50px',
+                border: '2.54439px solid rgba(234, 46, 16, 0.2)',
+                color: '#EA2E10',
+                fontWeight: 600,
+            }}>
+                <ShoppingCartIcon />
+                Add to Cart
+            </Button>
         </Box>
     );
 };
--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -1,11 +1,76 @@
-import { Container } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect } from 'react';
 import { useContext } from "react";
+import Slider from 'react-slick';
 import { DataContext } from './Context/DataProvider';
+// import '../Components/GlobalCss.css'
 import CourseCard from './CourseCard';
 import Content from './shared/Content';
 import Tagline from './shared/Tagline'
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+const Styles = {
+    color: 'red',
+}
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "red" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "green" }}
+            onClick={onClick}
+        />
+    );
+}
+const settings = {
+    dots: false,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+};
 const Courses = () => {
 
     const [courseList, setCourseList] = useContext(DataContext)
@@ -19,7 +84,6 @@ const Courses = () => {
         <div>
             <Container sx={{
                 mt: '50px',
-                backgroundColor: '#E5E5E5',
                 maxWidth: {
                     lg: 'lg',
                     xl: 'xl'
@@ -28,8 +92,21 @@ const Courses = () => {
                 <Tagline text={<div>Interacting First Aid Courses</div>} />
                 <Box>
                     <Content text="We are a company who is best known for offering awarding body accredited courses for anyone who wishes to take their professional life to the next level." />
+                    {/* <Box>
+                        <span className="slick-next "><ArrowRightAltIcon /></span>
+                        <span className="slick-prev "></span>
+                    </Box> */}
                 </Box>
-                <CourseCard />
+
+                <Box sx={{ backgroundColor: '#FEF9F7', mt: '30px', padding: '10px' }}>
+                    <Slider {...settings}>
+                        {courseList.map(course => (
+                            <CourseCard key={course.id} course={course} />
+                        ))}
+                    </Slider>
+                </Box>
+
+
             </Container>
         </div>
     );
