be8552c90e42dddc34d61fdba54fc0497e777329
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-02-09 12:33:13 +0600

    Hero Section Responsive(Beta) done

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,17 +1,145 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, Grid, Link, Typography } from '@mui/material';
 import React from 'react';
 import HeroLayout from './layout/HeroLayout';
+import PrimaryButton from './layout/PrimaryButton';
+import doctorImage from '../images/doctors.png'
+import first_aid_box from '../images/first-aid-box.png'
+import quiz_grid from '../images/grid3.png'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+const Styles = {
+    gridStyle: {
+        backgroundColor: '#FFF6F6',
+        position: 'relative',
+        overflow: 'hidden',
+        padding: '50px 20px'
+    }
+
+}
+
 
 const Hero = () => {
     return (
         <div>
             <Container maxWidth="xl">
 
-                <HeroLayout position={{
-                    bottom: '-90px',
-                    right: '-40px',
-                }} text={<div>Engaging & <br /> Accessible Online <br /> Course</div>}
-                />
+                <Box sx={{ width: '100%' }}>
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3, }} >
+                        <Grid item xs={12} lg={6} sx={Styles.gridStyle}>
+                            <Box sx={{ padding: '30px 10px' }}>
+                                <HeroLayout styling={{
+                                    bottom: '-111px',
+                                    right: '-40px',
+                                    fontSize: '55px',
+                                }} text={<div>Engaging & <br /> Accessible Online <br /> Course</div>}
+                                />
+                            </Box>
+                            <Box>
+                                <img src={doctorImage} alt=""
+                                    style={{
+                                        width: '50%',
+                                        position: 'absolute',
+                                        bottom: '0',
+                                        right: '0'
+                                    }} />
+                            </Box>
+
+                            <PrimaryButton text='Explore Now' />
+
+                        </Grid>
+                        <Grid item xs={12} lg={6} >
+                            <Box sx={Styles.gridStyle}>
+                                <HeroLayout styling={{
+                                    bottom: '-103px',
+                                    right: '-127px',
+                                    fontSize: '35px'
+                                }} text={<div>Trustable First Aid <br /> Courses Online</div>}
+                                />
+
+                                <Box component="span" sx={{ display: 'flex', alignItems: 'center', mt: { xs: 1, lg: 2 }, }}>
+                                    <PlayArrowIcon
+                                        sx={{
+                                            color: '#EA2E10',
+                                            backgroundColor: 'rgba(234, 46, 16, 0.1)',
+                                            padding: 1,
+                                            borderRadius: '50%',
+                                            mr: 1
+
+                                        }} />
+                                    <Typography sx={{
+                                        fontW: 600,
+                                        fontSize: '22px',
+                                        lineHeight: '36px',
+                                        color: '#272D4E'
+                                    }}>
+                                        Watch Promo
+                                    </Typography>
+                                </Box>
+
+                                <Box sx={{
+
+                                    position: 'absolute',
+                                    top: '60px',
+                                    right: {
+                                        xs: '-20px',
+                                        lg: '60px'
+                                    },
+                                }}>
+                                    <img src={first_aid_box} alt=""
+                                        style={{
+                                            width: '75%',
+
+
+                                        }} />
+                                </Box>
+
+                            </Box>
+                            <Box sx={{ mt: 3, ...Styles.gridStyle }}>
+                                <HeroLayout styling={{
+                                    bottom: '-103px',
+                                    right: '-127px',
+                                    fontSize: '35px'
+                                }} text={<div>Awesome Quizzes <br /> you can explore</div>}
+                                />
+                                <Box component="span" sx={{ display: 'flex', alignItems: 'center', mt: 2, }}>
+                                    <Box sx={{
+
+                                        position: 'absolute',
+                                        top: '60px',
+                                        right: {
+                                            xs: '-20px',
+                                            lg: '60px'
+                                        },
+                                    }}>
+                                        <img src={quiz_grid} alt=""
+                                            style={{
+                                                width: '75%',
+
+
+                                            }} />
+                                    </Box>
+                                    <Link sx={{
+                                        fontWeight: 'bold',
+                                        fontSize: '22px',
+                                        lineHeight: '36px',
+                                        color: '#EA2E10',
+                                        textDecoration: 'none',
+                                        cursor: 'pointer'
+                                    }}>
+                                        Explore Now
+                                    </Link>
+                                    <ArrowRightAltIcon
+                                        sx={{
+                                            color: '#EA2E10',
+                                            ml: 1
+                                        }} />
+
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Box>
+
 
             </Container>
         </div >
--- src/Components/layout/HeroLayout.js
+++ src/Components/layout/HeroLayout.js
@@ -1,60 +1,50 @@
 
-import { Box, Grid, Typography } from '@mui/material';
-import zIndex from '@mui/material/styles/zIndex';
-import React from 'react';
-import doctorImage from '../../images/doctors.png'
+import { Box, Typography } from '@mui/material';
 
-const HeroLayout = ({ position, text }) => {
+import React from 'react';
+
+
+const HeroLayout = ({ styling, text }) => {
     console.log();
     return (
         // backgroundColor: '#FFF6F6'
         <div>
 
-            <Box sx={{ width: '100%' }}>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} >
-                    <Grid item xs={6} sx={{ backgroundColor: '#FFF6F6', position: 'relative', overflow: 'hidden' }}>
 
-                        <Typography
-                            sx={{
-                                fontFamily: 'Circular Std',
-                                fontStyle: 'normal',
-                                fontWeight: 900,
-                                fontSize: '55px',
-                                lineHeight: '125%',
-                                color: '#272D4E',
-                                whiteSpace: 'pre-line'
-                            }}>
-                            {text}
-                        </Typography>
-                        <Box
-                            sx={{
-                                width: '350px',
-                                height: '350px',
-                                backgroundColor: '#FFE4E0',
-                                borderRadius: '50%',
-                                position: 'absolute',
-                                bottom: position.bottom,
-                                right: position.right,
+            <Typography
+                sx={{
+                    fontFamily: 'Circular Std',
+                    fontStyle: 'normal',
+                    fontWeight: 900,
+                    fontSize: {
+                        xs: '20px',
+                        lg: styling.fontSize,
+                    },
+                    lineHeight: '125%',
+                    color: '#272D4E',
+                    whiteSpace: 'pre-line',
+                    wordWrap: 'break-word',
+                }}>
+                {text}
+            </Typography>
+            <Box
+                sx={{
 
-                            }}
-                        >
-                            <img src={doctorImage} alt=""
-                                style={{
-                                    width: '80%',
-                                    position: 'absolute',
-                                    bottom: '91px',
-                                    right: '40px'
-                                }} />
+                    height: '450px',
+                    width: {
+                        xs: '300px',
+                        lg: '450px'
+                    },
+                    backgroundColor: '#FFE4E0',
+                    borderRadius: '50%',
+                    position: 'absolute',
+                    bottom: styling.bottom,
+                    right: styling.right,
 
-                        </Box>
+                }}
+            >
 
 
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Box sx={{ backgroundColor: '#FFF6F6', }}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptate dolor dolores molestiae dignissimos quaerat quod mollitia porro architecto, voluptates eligendi temporibus consectetur, iusto officiis itaque! A deserunt quisquam eligendi ipsa sapiente ullam eveniet hic ipsam velit? Est non deserunt natus voluptatum ex veniam vero nam corporis dolorum eligendi id, soluta similique. Doloremque facilis a dolore quidem aperiam vero perspiciatis est enim veniam laudantium, expedita velit necessitatibus quaerat, obcaecati voluptate eveniet debitis aut sapiente, reprehenderit ratione esse nostrum voluptas itaque. Beatae corrupti cum voluptatum, asperiores officia corporis praesentium neque at distinctio. Provident incidunt, molestiae illo necessitatibus explicabo vitae quo error.</Box>
-                        <Box sx={{ mt: 3, backgroundColor: '#FFF6F6', }}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptate dolor dolores molestiae dignissimos quaerat quod mollitia porro architecto, voluptates eligendi temporibus consectetur, iusto officiis itaque! A deserunt quisquam eligendi ipsa sapiente ullam eveniet hic ipsam velit? Est non deserunt natus voluptatum ex veniam vero nam corporis dolorum eligendi id, soluta similique. Doloremque facilis a dolore quidem aperiam vero perspiciatis est enim veniam laudantium, expedita velit necessitatibus quaerat, obcaecati voluptate eveniet debitis aut sapiente, reprehenderit ratione esse nostrum voluptas itaque. Beatae corrupti cum voluptatum, asperiores officia corporis praesentium neque at distinctio. Provident incidunt, molestiae illo necessitatibus explicabo vitae quo error.</Box>
-                    </Grid>
-                </Grid>
             </Box>
         </div >
     );
