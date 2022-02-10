50ef37432468eb3fb1331fcbd5caee6564cabc27
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-02-10 11:21:26 +0600

    Rating added to card and Tagline component fontWeight changed

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 import PeopleIcon from '@mui/icons-material/People';
 import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 import TagLine from './shared/Tagline';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-
+import StarIcon from '@mui/icons-material/Star';
 const CourseCard = ({ course }) => {
-    const { id, title, coverImage, enrolledStudents, rating, regularPrice } = course;
+    const { id, title, coverImage, enrolledStudents, rating, } = course;
     console.log(course);
     return (
         <Box sx={{
@@ -18,7 +18,24 @@ const CourseCard = ({ course }) => {
             boxShadow: '0px 45.799px 106.864px rgba(234, 46, 16, 0.06)',
             borderRadius: '19.0829px'
         }}>
-            <img style={{ width: '100%', marginBottom: '10px', borderRadius: '19.08px' }} src={coverImage} alt="" />
+
+            <Box position='relative'>
+
+                <Typography sx={{
+                    padding: '10px 20px',
+                    position: 'absolute',
+                    bottom: '5px',
+                    right: '5px',
+                    backgroundColor: '#FFFFFF',
+                    borderRadius: '6.36097px',
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                    <StarIcon sx={{ color: '#FFCB00' }} />
+                    <TagLine text={rating} fontSize='15px' fontWeight={500} />
+                </Typography>
+                <img style={{ width: '100%', marginBottom: '10px', borderRadius: '19.08px' }} src={coverImage} alt="" />
+            </Box>
             <TagLine text={<div>{title}</div>} fontSize="20px" />
 
             <Box sx={{
--- src/Components/shared/Tagline.js
+++ src/Components/shared/Tagline.js
@@ -1,14 +1,14 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 
-const TagLine = ({ fontSize, text }) => {
+const TagLine = ({ fontSize, fontWeight, text }) => {
     return (
         <div>
             <Typography
                 sx={{
                     fontFamily: 'Circular Std',
                     fontStyle: 'normal',
-                    fontWeight: 900,
+                    fontWeight: 900 || { fontWeight },
                     fontSize: {
                         xs: '20px',
                         lg: fontSize || '55px',
