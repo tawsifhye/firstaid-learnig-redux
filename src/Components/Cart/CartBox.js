e4aa002c35f4e111cce39e68e2974b15126a3028
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-02-16 11:31:30 +0600

    cart item remove added

--- src/Components/Cart/CartBox.js
+++ src/Components/Cart/CartBox.js
@@ -1,49 +1,61 @@
 import { Box, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import React, { useContext } from 'react';
+import ClearIcon from '@mui/icons-material/Clear';
 import { DataContext } from '../../Context/DataProvider';
 
 const CartBox = () => {
 
-    const [cart] = useContext(DataContext);
+    const [cart, setCart] = useContext(DataContext);
+    console.log(cart);
+
+    const deleteItem = (item) => {
+        const newCart = cart.filter(cart => (cart.id !== item.id))
+        setCart(newCart);
+    }
+
     return (
 
         <Box sx={{ backgroundColor: '#EDF5FF', py: '50px' }}>
             <Container sx={{
                 background: '#FFFFFF',
-                borderRadius: '10px'
+                borderRadius: '10px',
+                width: 550
             }}>
                 <Typography variant='h4'>Shopping Cart</Typography>
 
-                <TableContainer >
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Dessert (100g serving)</TableCell>
-                                <TableCell align="right">Calories</TableCell>
-                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {cart.map((item) => (
-                                <TableRow
-                                    key={item.id}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {item.name}
-                                    </TableCell>
-                                    <TableCell align="right">{ }</TableCell>
-                                    <TableCell align="right">{ }</TableCell>
-                                    <TableCell align="right">{ }</TableCell>
-                                    <TableCell align="right">{ }</TableCell>
+                <Box>
+                    <TableContainer >
+                        <Table sx={{ width: 450 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{ fontWeight: 'bold' }}>Course Name</TableCell>
+                                    <TableCell align="right" sx={{ fontWeight: 'bold' }}>Price</TableCell>
+                                    <TableCell align="right" sx={{ fontWeight: 'bold' }}>Quantity</TableCell>
+                                    <TableCell align="right" sx={{ fontWeight: 'bold' }}>Total</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                            </TableHead>
+                            <TableBody>
+                                {cart.map((item) => (
+                                    <TableRow
+                                        key={item.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {item.title}
+                                        </TableCell>
+                                        <TableCell align="right">${item.regularPrice}</TableCell>
+                                        <TableCell align="right">
+                                            {item.quantity}
+                                        </TableCell>
+                                        <TableCell align="right">${parseFloat(item.regularPrice - item.regularPrice * .75) * item.quantity.toFixed(2)}</TableCell>
+                                        <TableCell align="right"><ClearIcon onClick={() => deleteItem(item)} /> </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
 
+                </Box>
             </Container>
 
         </Box>
