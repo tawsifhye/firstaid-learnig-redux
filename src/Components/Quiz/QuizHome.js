db7d008789160fd9f7689b2cf8f38cc178f9329d
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-02-17 12:08:21 +0600

    quiz count added

--- src/Components/Quiz/QuizHome.js
+++ src/Components/Quiz/QuizHome.js
@@ -24,9 +24,12 @@ const Styles = {
 const QuizHome = () => {
     const [quizes, setQuizes] = useState([]);
     const [selected, setSelected] = useState(false);
-    const [correctAnswer, setCorrectAnswer] = useState(0);
+    const [correctAnswer, setCorrectAnswer] = useState([]);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [selectedAnswer, setSelectedAnswer] = useState([]);
     let correctAnswerVar = 0;
     let rightAnswer = [];
+    // let selectedAnswer = [];
     useEffect(() => {
         fetch('quiz.json')
             .then(res => res.json())
@@ -36,9 +39,14 @@ const QuizHome = () => {
 
     // });
     quizes.forEach((quiz) => (
-        rightAnswer?.push(quiz.right_answer)
+        rightAnswer?.push(
+            {
+                id: quiz?.id,
+                rightAnswer: quiz?.right_answer
+            }
+        )
     ))
-    // console.log(rightAnswer);
+    // console.log('rightAnswer', rightAnswer);
     const theme = useTheme();
     const [activeStep, setActiveStep] = React.useState(0);
     const maxSteps = quizes.length;
@@ -53,93 +61,114 @@ const QuizHome = () => {
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
-    console.log(correctAnswer);
-    const checkAnswer = (e) => {
-        if (e.target.value === quizes[activeStep].right_answer) {
-            correctAnswerVar = correctAnswer + 1;
-            setCorrectAnswer(correctAnswerVar)
 
-        } else if (correctAnswer > 0) {
-            correctAnswerVar = correctAnswer - 1;
-            setCorrectAnswer(correctAnswerVar)
+    const checkAnswer = (e) => {
+        setSelected(true);
+        const answer = {
+            id: activeStep + 1,
+            selectedAnswer: e.target.value
         }
-        // console.log(e.target.value);
-        // console.log(quizes[activeStep].right_answer);
-        if (e.target.checked) {
-            setSelected(true);
-        }
-        else {
-            setSelected(false);
-        }
-        // if (e.target.value === ) { }
-        // else { }
+        const newArr = [...selectedAnswer, answer];
+        setSelectedAnswer(newArr);
     }
+
+    const handleSubmit = () => {
+        const findCorrect = selectedAnswer.map(a => (rightAnswer.filter(x => (a.id == x.id && a.selectedAnswer == x.rightAnswer))))
+        // console.log('Correct answers', findCorrect);
+        const getSelected = rightAnswer.map(a => (selectedAnswer.filter(x => (a.id == x.id && a.selectedAnswer == x.rightAnswer))))
+        console.log('selected by user', getSelected);
+        // const selectedFinder = getSelected.filter(a => (a[a.length - 1]))
+        // console.log(selectedFinder);
+
+        const filter = findCorrect.filter(a => (a.length))
+        setCorrectAnswer(filter);
+        setIsSubmitted(true);
+    }
+    console.log('Correct answers', correctAnswer);
+    // console.log('selectedAnswer', selectedAnswer);
     return (
         <Box>
             <Container>
                 <Typography variant='h3' sx={{ textAlign: 'center' }}>Free Quizes!</Typography>
                 <br /> <br />
-                <Box sx={{ width: "100%", flexGrow: 1 }}>
-                    <Paper
-                        square
-                        elevation={0}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            pl: 2,
-                            bgcolor: 'background.default',
-                        }}
-                    >
-                        <Typography>{quizes[activeStep]?.question}</Typography>
-                    </Paper>
-                    <Box sx={{ width: '80%', p: 2 }}>
+
+                {!isSubmitted &&
+                    <Box sx={{ width: "100%", flexGrow: 1 }}>
+                        <Paper
+                            square
+                            elevation={0}
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                pl: 2,
+                                bgcolor: 'background.default',
+                            }}
+                        >
+                            <Typography>{quizes[activeStep]?.question}</Typography>
+                        </Paper>
+                        <Box sx={{ width: '80%', p: 2 }}>
 
 
 
-                        <FormControl>
-                            <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                name="radio-buttons-group"
-                            >
-                                {quizes[activeStep]?.options.map((answer) => (
-                                    <FormControlLabel key={answer.id} value={answer?.id} control={<Radio />} label={answer?.option} onChange={checkAnswer} />
-                                ))}
-                            </RadioGroup>
-                        </FormControl>
+                            <FormControl>
+                                <RadioGroup
+                                    aria-labelledby="demo-radio-buttons-group-label"
+                                    name="radio-buttons-group"
+                                >
+                                    {quizes[activeStep]?.options.map((answer) => (
+                                        <FormControlLabel key={answer.id} value={answer?.id} control={<Radio />} label={answer?.option} onChange={checkAnswer} />
+                                    ))}
+                                </RadioGroup>
+                            </FormControl>
 
 
+                        </Box>
+                        <MobileStepper
+                            variant="text"
+                            steps={maxSteps}
+                            position="static"
+                            activeStep={activeStep}
+                            nextButton={
+                                <Button
+                                    size="small"
+                                    onClick={handleNext}
+                                    disabled={activeStep === maxSteps - 1 || !selected}
+                                >
+                                    Next
+                                    {theme.direction === 'rtl' ? (
+                                        <KeyboardArrowLeft />
+                                    ) : (
+                                        <KeyboardArrowRight />
+                                    )}
+                                </Button>
+                            }
+                            backButton={
+                                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                                    {theme.direction === 'rtl' ? (
+                                        <KeyboardArrowRight />
+                                    ) : (
+                                        <KeyboardArrowLeft />
+                                    )}
+                                    Back
+                                </Button>
+                            }
+                        />
+                        {
+                            activeStep === quizes.length - 1 &&
+                            <Button onClick={handleSubmit} variant="contained">Submit</Button>
+                        }
                     </Box>
-                    <MobileStepper
-                        variant="text"
-                        steps={maxSteps}
-                        position="static"
-                        activeStep={activeStep}
-                        nextButton={
-                            <Button
-                                size="small"
-                                onClick={handleNext}
-                                disabled={activeStep === maxSteps - 1 || !selected}
-                            >
-                                Next
-                                {theme.direction === 'rtl' ? (
-                                    <KeyboardArrowLeft />
-                                ) : (
-                                    <KeyboardArrowRight />
-                                )}
-                            </Button>
-                        }
-                        backButton={
-                            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                                {theme.direction === 'rtl' ? (
-                                    <KeyboardArrowRight />
-                                ) : (
-                                    <KeyboardArrowLeft />
-                                )}
-                                Back
-                            </Button>
-                        }
-                    />
-                </Box>
+                }
+                {
+                    isSubmitted &&
+                    <Box>
+                        <Paper elevation={3} sx={{ textAlign: 'center' }}>
+                            <Typography variant='h4' > Your Correct Answer: {correctAnswer.length}/{quizes.length}</Typography>
+                            <br /> <br />
+                            <Typography variant='h5' > Suggested Courses</Typography>
+                        </Paper>
+                    </Box>
+                }
 
             </Container>
         </Box>
